4e995f751d14b0429003aed84dfbac08bc34da45
siharold918 <126165482+siharold918@users.noreply.github.com>
2023-02-23 21:02:18 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("download-button").href = "path/to/file.txt";
+
+document.getElementById("download-button").addEventListener("click", function(){
+    
+  });
+
+  
---
+++ js/languages.js
@@ -0,0 +1,20 @@
+const languages = [
+    {
+      no: "1",
+      name: "English",
+      native: "English",
+      code: "en",
+    },
+      {
+        no: "2",
+        name: "Ilocano",
+        native: "Iloko",
+        code: "ilo",
+      },
+      {
+        no: "3",
+        name: "Filipino",
+        native: "Filipino",
+        code: "tl",
+      },
+    ];a
